a40031a49c9ed2b249834e7e4b10f51f2a1085c7
Wout <hi@wout.codes>
2025-11-21 11:07:35 +0100

    feat: add tag

---
+++ src/lucky_honeypot/tag.cr
@@ -0,0 +1,19 @@
+module LuckyHoneypot::Tag
+  macro honeypot_input(name, **named_args)
+    context.session.set(
+      "{{LuckyHoneypot::SESSION_KEY_PREFIX.id}}_{{name.gsub(/\:/, "_").id}}",
+      Time.utc.to_unix_ms.to_s
+    )
+    input(
+      name: {{name}},
+      type: "text",
+      aria_hidden:  true,
+      tabindex:     -1,
+      autocomplete: "off",
+      {% unless named_args.has_key?(:class) || named_args.has_key?(:style) %}
+        style: "position:absolute;left:-9999px;width:1px;height:1px;pointer-events:none;",
+      {% end %}
+      {{named_args.double_splat}}
+    )
+  end
+end
