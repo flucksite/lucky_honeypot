0696b85bef4c2ae367fe1e75e268843ddbb866f9
Wout <hi@wout.codes>
2025-11-21 11:07:46 +0100

    test: add tag specs

---
+++ spec/lucky_honeypot/tag_spec.cr
@@ -0,0 +1,88 @@
+require "lucky"
+require "../spec_helper"
+
+describe LuckyHoneypot::Tag do
+  describe "#honeypot_input" do
+    it "renders a honeypot with a default style attibute" do
+      html = LuckyHoneypotTagWithStyleTestPage.new(test_context).render.to_s
+
+      html.should contain(%(name="user:name" type="text"))
+      html.should contain(%(aria-hidden="true" tabindex="-1" autocomplete="off"))
+      html.should contain(
+        %(style="position:absolute;left:-9999px;width:1px;height:1px;pointer-events:none;")
+      )
+    end
+
+    it "sets a timestamp in the session" do
+      page = LuckyHoneypotTagWithStyleTestPage.new(test_context)
+      page.render.to_s
+
+      puts page.context.session.inspect
+      timestamp = page.context.session.get(:honeypot_timestamp_user_name)
+      timestamp.size.should eq(13)
+      timestamp.to_i64.should be <= Time.utc.to_unix_ms
+    end
+
+    it "renders a honeypot with a class and no style attibute" do
+      html = LuckyHoneypotTagWithClassTestPage.new(test_context).render.to_s
+
+      html.should contain(%(class="visually-hidden"))
+      html.should_not contain(%(style="))
+    end
+
+    it "renders a honeypot with a custom style attibute" do
+      html = LuckyHoneypotTagWithCustomStyleTestPage.new(test_context).render.to_s
+
+      html.should contain(%(style="display:none"))
+      html.should_not contain(%(style="position:absolute;left:-9999px))
+    end
+
+    it "renders a honeypot with a additional attributes" do
+      html = LuckyHoneypotTagWithAdditionalAttributesTestPage.new(test_context).render.to_s
+
+      html.should contain(%(data-very="lukcy"))
+    end
+  end
+end
+
+private abstract class LuckyHoneypotBaseTestPage
+  include Lucky::HTMLPage
+  include LuckyHoneypot::Tag
+
+  abstract def render
+end
+
+private class LuckyHoneypotTagWithStyleTestPage < LuckyHoneypotBaseTestPage
+  def render
+    honeypot_input("user:name")
+    view
+  end
+end
+
+private class LuckyHoneypotTagWithClassTestPage < LuckyHoneypotBaseTestPage
+  def render
+    honeypot_input("user:name", class: "visually-hidden")
+    view
+  end
+end
+
+private class LuckyHoneypotTagWithCustomStyleTestPage < LuckyHoneypotBaseTestPage
+  def render
+    honeypot_input("user:name", style: "display:none")
+    view
+  end
+end
+
+private class LuckyHoneypotTagWithAdditionalAttributesTestPage < LuckyHoneypotBaseTestPage
+  def render
+    honeypot_input("user:name", data_very: "lukcy")
+    view
+  end
+end
+
+private def test_context : HTTP::Server::Context
+  io = IO::Memory.new
+  request = HTTP::Request.new("GET", "/")
+  response = HTTP::Server::Response.new(io)
+  HTTP::Server::Context.new request, response
+end
---
+++ spec/spec_helper.cr
@@ -0,0 +1,7 @@
+require "spec"
+require "lucky"
+require "../src/lucky_honeypot"
+
+Lucky::Session.configure do |settings|
+  settings.key = "juy8u/D9exRhJZXp+NObvdibNdSike0s16dHJCTmT2E="
+end
