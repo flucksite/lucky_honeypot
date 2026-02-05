1d28509886e31f4c491309c0b6a47b4cc30de2a3
Wout <hi@wout.codes>
2026-02-05 17:09:24 +0100

    feat: add signals object

---
+++ src/lucky_honeypot/signals.cr
@@ -0,0 +1,20 @@
+require "json"
+
+struct LuckyHoneypot::Signals
+  include JSON::Serializable
+
+  # Mouse movement
+  getter m : Bool
+  # touch gesture
+  getter t : Bool
+  # Scroll trigger
+  getter s : Bool
+  # Keyboard input
+  getter k : Bool
+
+  # Calculates the likelyhood of a human (0 being a bot, 1 being a human). Any
+  # value above 0 is already a good chance.
+  def human_rating : Float64
+    [m, t, s, k].count { |f| f } / 4
+  end
+end
