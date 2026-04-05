42dc53eca8d348861c0e05bebe2bf80687a197a7
Wout <hi@wout.codes>
2026-04-05 18:40:19 +0200

    fix: typo in signals

--- src/lucky_honeypot/signals.cr
+++ src/lucky_honeypot/signals.cr
@@ -17,10 +17,10 @@ struct LuckyHoneypot::Signals
   # Calculates the likelyhood of a human (0 being a bot, 1 being a human). Any
   # value above 0 is already a good chance.
   def human_rating : Float64
-    [mouse?, touch?, scroll?, keyboard?, focus?].count(&.itself) / 5
+    [mouse?, touch?, scroll?, keyboard?, focus?].count(&.itself) / 5.0
   end
 
-  # Convenience method to caclulate the human rating form a class method.
+  # Convenience method to calculate the human rating for a class method.
   def self.human_rating(json : String) : Float
     from_json(json).human_rating
   end
